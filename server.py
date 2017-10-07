735ce062565d5ff2d9c2e51b9040793abe93a7de
nevix <gal. nevis@gmail.com>
2017-10-07 12:52:26 +0300

    add calculation

--- server.py
+++ server.py
@@ -2,12 +2,14 @@ import random
 
 from drone.consts import SENSOR_MODE_PASSIVE_TRACKING
 from drone.entities import Sensor
+from drone import geo_base
 
 
 def listen():
     sensor1 = Sensor(1, 2)
     sensor2 = Sensor(3, 4)
     sensor3 = Sensor(6, 10)
+    landing_site = LandingSite(0, 0)
 
     # detached..
     sensor1.scan_perimeter()
@@ -24,16 +26,29 @@ def listen():
             if is_a_threat(sensors[idx]):
                 threat_detected = True
         if threat_detected:
-            handle_threat(choose_sensor(possible_sensors_for_handling))
+            handle_threat(choose_sensor(possible_sensors_for_handling, landing_site))
 
 
 def handle_threat(sensor):
     sensor.land_drone()
 
 
-def choose_sensor(possible_sensors_for_handling):
-    # todo add policy here
-    return random.choice(possible_sensors_for_handling)
+def choose_sensor(possible_sensors_for_handling, landing_site):
+    # based on the shortest path from drone to destination - choose the closest sensor to middle of path
+    middle_of_route_post = calculate_drone_to_landing_site_path_middle_coordinates(possible_sensors_for_handling.
+                                                                                   tracked_drone_movement[:-1], landing_site)
+    distances_from_sensors_to_middle_of_path = []
+    for sensor in possible_sensors_for_handling:
+        distances_from_sensors_to_middle_of_path.append(sensor.calculate_distance_from_object(middle_of_route_post))
+    minimum_distance = min(distances_from_sensors_to_middle_of_path)
+    minimum_idx = distances_from_sensors_to_middle_of_path.index(minimum_distance)
+    return possible_sensors_for_handling[minimum_idx]
+
+
+def calculate_drone_to_landing_site_path_middle_coordinates(drone, landing_site):
+    middle_lat = ((drone.latitude - landing_site.latitude) / 2) + landing_site.latitude
+    middle_lon = ((drone.longitude - landing_site.longitude) / 2) + drone.longitude
+    return geo_base.GeoEntity(middle_lat, middle_lon)
 
 
 def is_a_threat(sensor):
